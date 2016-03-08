b7ef204266ba303bad61169a0b9ce05811f0b754
rebeccabillings <rbillings@mozilla.com>
2016-03-08 13:39:13 -0800

    Added tests for pinning via logviewer and clearing the pinboard

--- tests/test_pin_job.py
+++ tests/test_pin_job.py
@@ -8,16 +8,16 @@ from pages.treeherder import TreeherderPage
 class TestPinJobs:
 
     def test_pin_next_job(self, base_url, selenium):
-        """ Open treeherder page, select next job and pin it"""
+        # Open treeherder page, select next job and pin it
         page = TreeherderPage(base_url, selenium).open()
         current_job_title = page.select_next_job()
         assert 0 == page.pinboard.pins
-        page.result.pin_using_spacebar()
+        page.pin_using_spacebar()
         assert 1 == page.pinboard.pins
         assert current_job_title in page.pinboard.pinned_job_title
 
     def test_pin_job_from_job_details(self, base_url, selenium):
-        """ Open treeherder page, select next job, pin it by the logviewer icon"""
+        # Open treeherder page, select next job, pin it by the logviewer icon
         page = TreeherderPage(base_url, selenium).open()
         job = page.result_sets[0].jobs[0]
         job.click()
@@ -25,6 +25,7 @@ class TestPinJobs:
         page.job_details.pin_job()
         assert 1 == len(page.pinboard.jobs)
         assert job.symbol == page.pinboard.selected_job.symbol
+
 
     def test_clear_pinboard(self, base_url, selenium):
         """ Open treeherder page, pin a job and then clear the pinboard"""
