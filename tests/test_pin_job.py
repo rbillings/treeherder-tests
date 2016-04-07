d553d1d2259faea19583f142980843a70007aaff
rebeccabillings <rbillings@mozilla.com>
2016-04-07 12:51:33 -0700

    Fixed merge conflicts

--- tests/test_pin_job.py
+++ tests/test_pin_job.py
@@ -17,7 +17,7 @@ class TestPinJobs:
         assert current_job_title in page.pinboard.pinned_job_title
 
     def test_pin_job_from_job_details(self, base_url, selenium):
-        # Open treeherder page, select next job, pin it by the logviewer icon
+        """ Open treeherder page, select next job, pin it by the logviewer icon"""
         page = TreeherderPage(base_url, selenium).open()
         job = page.result_sets[0].jobs[0]
         job.click()
@@ -27,15 +27,16 @@ class TestPinJobs:
         assert job.symbol == page.pinboard.selected_job.symbol
 
     def test_clear_pinboard(self, base_url, selenium):
-        """ Open treeherder page, pin a job and then clear the pinboard"""
+        """ Open treeherder page, pin a job and then clear the pinboard """
         page = TreeherderPage(base_url, selenium).open()
+
         page.select_next_job()
-        page.result.pin_using_spacebar()
+        page.pin_using_spacebar()
         assert 1 == page.pinboard.pins
         page.pinboard.clear_pinboard()
         assert page.pinboard.is_pinboard_open
         assert 0 == page.pinboard.pins
-
+    
     def test_pin_all_jobs(self, base_url, selenium):
         """ Open treeherder page, pin all jobs, confirm no more than 500 pins in pinboard"""
         page = TreeherderPage(base_url, selenium).open()
