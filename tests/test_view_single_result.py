5882d624ff1a9334760163178d31b7af55959a01
rebeccabillings <rbillings@mozilla.com>
2016-10-19 10:00:13 -0700

    edited single result test

--- tests/test_view_single_result.py
+++ tests/test_view_single_result.py
@@ -11,6 +11,7 @@ from pages.treeherder import TreeherderPage
 def test_open_single_result(base_url, selenium):
     page = TreeherderPage(selenium, base_url).open()
     assert page.results_visible
+    assert len(page.result_sets) > 1
     datestamp = page.result_sets[0].datestamp
     page.result_sets[0].view()
     assert 1 == len(page.result_sets)
