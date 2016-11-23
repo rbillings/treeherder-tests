7d9beb48cc8d50e71d96c63967b2b8390c5f571c
rebeccabillings <rbillings@mozilla.com>
2016-11-23 08:51:49 -0800

    Adjusted resultsset locator and page load method

--- pages/treeherder.py
+++ pages/treeherder.py
@@ -31,14 +31,15 @@ class TreeherderPage(Base):
     _quick_filter_locator = (By.ID, 'quick-filter')
     _repos_menu_locator = (By.ID, 'repoLabel')
     _result_sets_locator = (By.CSS_SELECTOR, '.result-set:not(.row)')
-    _resultset_visible_locator = (By.CSS_SELECTOR, 'span.hidden.ready')
+    _resultset_visible_locator = (By.CSS_SELECTOR, 'span.hidden' )
     _unchecked_repos_links_locator = (By.CSS_SELECTOR, '#repoLabel + .dropdown-menu .dropdown-checkbox:not([checked]) + .dropdown-link')
     _unclassified_failure_count_locator = (By.ID, 'unclassified-failure-count')
     _unclassified_failure_filter_locator = (By.CSS_SELECTOR, '.btn-unclassified-failures')
 
     def wait_for_page_to_load(self):
         el = self.find_element(*self._resultset_visible_locator)
-        self.wait.until(EC.visibility_of(el))
+        self.wait.until(lambda s: 'hidden' in el.get_attribute('class'))
+        return self
 
     @property
     def active_watched_repo(self):
